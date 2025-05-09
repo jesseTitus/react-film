7a399271f68e0dbf1508c687c44bc8872a8a1347
jesseTitus <jessejtitus@gmail.com>
2025-05-09 17:00:09 -0400

    ...

--- react-film/src/App.tsx
+++ react-film/src/App.tsx
@@ -1,16 +1,21 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
-
+import { useState } from "react";
+import Counter from "./components/Counter";
 function App() {
   // let items = ["New York", "San fafjldkasf", "London", "Italy"];
   // const handleSelectItem = (item: string) => {
   //   console.log(item);
   // };
+  const [alertVisible, setAlertVisibility] = useState(false); //state hook: used to only show alert if true
   return (
     <div>
-      <Alert children="Hello world" />
-      <Button onClick={() => console.log("clicked")}>My button</Button>
+      <Counter />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)} children="Alert" />
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My button</Button>
     </div>
   );
 }
